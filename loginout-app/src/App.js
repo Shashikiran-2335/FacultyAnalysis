d1b32c086cd18d857aea0cd7fac1e32ca59ba4a9
Rajasomeshwar <rajasomeshwarbitla@gmail.com>
2024-06-22 18:22:05 +0530

    login and logout

---
+++ loginout-app/src/App.js
@@ -0,0 +1,11 @@
+import LoginCompoment from "./LoginCompoment/LoginCompoment";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginCompoment />
+    </div>
+  );
+}
+
+export default App;
